a72ee1721cb0606e3e156f9caaefc16f3b1c0ff1
JoseH-Git <joseh.sierra@outlook.com>
2021-06-11 15:38:38 -0400

    Mars Hemispheres

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,16 +1,17 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
+import requests
 
 
 def scrape():
-    # browser = init_browser()
+    #Mars News
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
     listings = {}
 
-    url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    url = "https://redplanetscience.com"
     browser.visit(url)
     time.sleep(1)
 
